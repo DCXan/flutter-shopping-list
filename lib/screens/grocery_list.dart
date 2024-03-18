24b7887ddb00695385f16cae05c0efc849f044f5
David Chen <chen.david.1016@gmail.com>
2024-03-18 15:42:55 -0500

    removed dummy list data

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shopping_list/data/dummy_items.dart';
+import 'package:flutter_shopping_list/models/grocery_item.dart';
 import 'package:flutter_shopping_list/widgets/list_item.dart';
 import 'package:flutter_shopping_list/widgets/new_item.dart';
 
@@ -11,14 +11,22 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final items = groceryItems;
+  final List<GroceryItem> _groceryItems = [];
 
-  void _addItem() {
-    Navigator.of(context).push(
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   @override
@@ -26,12 +34,17 @@ class _GroceryListState extends State<GroceryList> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
-        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
-        itemCount: items.length,
+        itemCount: _groceryItems.length,
         itemBuilder: ((context, index) {
-          var item = items[index];
+          var item = _groceryItems[index];
           return ListItem(item: item);
         }),
       ),
