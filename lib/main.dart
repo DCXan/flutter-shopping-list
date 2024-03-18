2b58cd367e0e2441a82f32f5cfba759e2b87446d
David Chen <chen.david.1016@gmail.com>
2024-03-17 22:14:55 -0500

    add new item form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shopping_list/screens/widgets/grocery_list.dart';
+import 'package:flutter_shopping_list/screens/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_list/data/dummy_items.dart';
-import 'package:flutter_shopping_list/screens/widgets/list_item.dart';
+import 'package:flutter_shopping_list/widgets/list_item.dart';
+import 'package:flutter_shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -12,11 +13,20 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   final items = groceryItems;
 
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
         itemCount: items.length,
