6c0f0840e5d8bfed80896dbdc49ab0db18730a2e
David Chen <chen.david.1016@gmail.com>
2024-03-20 22:13:26 -0500

    close new item screen after 200 response from db

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -20,28 +20,31 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.other]!;
 
-  _saveItem() {
+  _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'flutter-grocery-list-46ae2-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(url,
-          headers: {'Content-Type': 'application/json'},
-          body: json.encode({
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(
+          {
             'name': _enteredName,
             'quantity': _enteredQuantity,
             'category': _selectedCategory.title,
-          }));
-
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+          },
         ),
       );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
