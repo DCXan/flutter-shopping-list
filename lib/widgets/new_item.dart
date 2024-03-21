7c7c35012fb5d084ce9236d54887024e347dac6d
David Chen <chen.david.1016@gmail.com>
2024-03-20 19:29:39 -0500

    add http pkg and firebase url

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_list/data/categories.dart';
 import 'package:flutter_shopping_list/models/category.dart';
 import 'package:flutter_shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -20,6 +23,17 @@ class _NewItemState extends State<NewItem> {
   _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      final url = Uri.https(
+          'flutter-grocery-list-46ae2-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }));
+
       Navigator.of(context).pop(
         GroceryItem(
           id: DateTime.now().toString(),
